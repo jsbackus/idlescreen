5f30dc5a2d6361fe583f9633465b848700732d62
Jeff Backus <jeff.backus@gmail.com>
2008-12-25 20:22:40 +0000

    Added the framework for Crawlies.  It is a copy of PlasmaLenz.  All file names have been modified as necessary, however the code hasn't been touched.  MSVC project files and solution will need to be generated. Had to delete them because each project is given a unique project ID.

---
+++ Crawlies/src/ProfileEditDialog.h
@@ -0,0 +1,117 @@
+/**
+ * Copyright (c) 2008 Jeff Backus.
+ *
+ * Licensed under the GNU General Public License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Additionally, this code makes heavy use of the Qt libraries from
+ * Trolltech.  These libraries are released under the GNU
+ * General Public License, Version 2.0, as well.  Please contact
+ * Trolltech for more information.
+ *
+ *     http://trolltech.com/
+ * 
+ */
+
+/**
+ * This class represents the dialog box used to edit the screen saver profiles.
+ * Manages both the MasterProfile and BackgroundProfile objects.
+ * 
+ * Revision History:
+ * 080301	jsbackus	Initial Revision.
+ */
+
+#ifndef __PROFILEEDITDIALOG_H__
+#define __PROFILEEDITDIALOG_H__
+
+#include <QDialog>
+#include <QListWidget>
+#include <QComboBox>
+#include <QCheckBox>
+#include <QLineEdit>
+#include <QSpinBox>
+#include <QDoubleSpinBox>
+#include <QSlider>
+
+//#include "../../common/2d_bgnd_w_lens/ConfigManager.h"
+//#include "../../common/2d_bgnd_w_lens/MasterProfile.h"
+//#include "../../common/2d_bgnd_w_lens/BackgroundProfile.h"
+//#include "PlasmaFractalBackgroundProfile.h"
+//#include "../../../common/IndexedPalette/IndexedPaletteDialog.h"
+
+#include "2d_bgnd_w_lens/ConfigManager.h"
+#include "2d_bgnd_w_lens/MasterProfile.h"
+#include "2d_bgnd_w_lens/BackgroundProfile.h"
+#include "PlasmaFractalBackgroundProfile.h"
+#include "IndexedPalette/IndexedPaletteDialog.h"
+
+class ProfileEditDialog : public QDialog {
+
+	Q_OBJECT
+
+public:
+	ProfileEditDialog(QString targetName = "", ConfigManager* confMgr = NULL, QWidget* parent = 0, Qt::WindowFlags f = 0);
+	~ProfileEditDialog();
+
+public slots:
+	//ok is responsible for updating the ConfigManager
+	void okClicked(bool checked = false);
+	void cancelClicked(bool checked = false);
+	void helpClicked(bool checked = false);
+
+	void addPalClicked(bool checked = false);
+	void editPalClicked(bool checked = false);
+	void animatePalClicked(bool checked = false);
+
+signals:
+	void profileNameChange(QString oldName, QString newName);
+
+private:
+	//attempts to edit the specified palette.
+	//void editPalette(IndexedPaletteProfile* pal, bool bWasAddRename = false);
+	//attempts to edit the specified palette.
+	void editPalette(const QString& palName, bool bReplace = false);
+
+	// populates the palette name selection combo box
+	void populatePalList(QString selected);
+
+	//MasterProfile related
+	QLineEdit* _nameBox;
+	QSpinBox* _timerBox;
+	QComboBox* _screenXBox;
+	QComboBox* _screenYBox;
+
+	MasterProfile _mp;
+
+	//PlasmaFractalBackgroundProfile related
+	QSlider* _coarsenessBox;
+	float _coarsenessAdjust;
+	QComboBox* _paletteBox;
+	bool _bAnimatePalette;
+	QDoubleSpinBox* _paletteXSpeedBox;
+	QDoubleSpinBox* _paletteYSpeedBox;
+	QCheckBox* _clampColorBox;
+
+	IndexedPaletteDialog* _palDlg;
+	QString _palOldName;
+
+	// lens related
+	QSlider* _lensSizeSlider;
+	QSlider* _lensVarSlider;
+	QSpinBox* _numLenses;
+
+	ConfigManager* _confMgr;
+
+	QString _oldName;
+	QString _windowTitle;
+};
+#endif
---
+++ Crawlies/src/project_specific_externs.cpp
@@ -0,0 +1,114 @@
+/**
+ * Copyright (c) 2008 Jeff Backus.
+ *
+ * Licensed under the GNU General Public License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Additionally, this code makes heavy use of the Qt libraries from
+ * Trolltech.  These libraries are released under the GNU
+ * General Public License, Version 2.0, as well.  Please contact
+ * Trolltech for more information.
+ *
+ *     http://trolltech.com/
+ * 
+ */
+
+/**
+ * This file contains project specific definitions, including version number.
+ */
+
+#include <QString>
+
+#include "../../common/2d_bgnd_w_lens/BackgroundProfile.h"
+#include "../../common/2d_bgnd_w_lens/lens_engine/LensProfile.h"
+#include "../../common/2d_bgnd_w_lens/lens_engine/SphericalLensProfile.h"
+#include "PlasmaFractalBackgroundProfile.h"
+
+/**
+ * This function will create an array that contains the profiles
+ * for allowable backgrounds and sets numProfiles to indicate
+ * the number of profiles in the array.  If there is a problem,
+ * numProfiles will be set to 0 and NULL will be returned.
+ */
+BackgroundProfile** getBackgroundTypes(int* numProfiles) {
+	BackgroundProfile** retVal = NULL;
+
+	*numProfiles = 1;
+	retVal = new BackgroundProfile*[*numProfiles];
+
+	if(retVal == NULL) {
+		*numProfiles = 0;
+		return NULL;
+	}
+
+	retVal[0] = (BackgroundProfile*) (new PlasmaFractalBackgroundProfile());
+
+	if(retVal[0] == NULL) {
+		*numProfiles = 0;
+		return NULL;
+	}
+
+	return retVal;
+}
+
+/**
+ * This function will create an array that contains the profiles
+ * for allowable lenses and sets numProfiles to indicate
+ * the number of profiles in the array.  If there is a problem,
+ * numProfiles will be set to 0 and NULL will be returned.
+ */
+LensProfile** getLensTypes(int* numProfiles) {
+	LensProfile** retVal = NULL;
+
+	*numProfiles = 1;
+	retVal = new LensProfile*[*numProfiles];
+
+	if(retVal == NULL) {
+		*numProfiles = 0;
+		return NULL;
+	}
+
+	retVal[0] = (LensProfile*) (new SphericalLensProfile());
+
+	if(retVal[0] == NULL) {
+		*numProfiles = 0;
+		return NULL;
+	}
+
+	return retVal;
+}
+
+/**
+ * This function returns the string used to create the path for
+ * the settings file.  It is also used to define the XML
+ * document root.
+ */
+QString getAppConfigName(){
+	return QString("PlasmaLenz");
+}
+
+/**
+ * Returns the full name of the screen saver to be used in
+ * places such as title bars of dialog boxes and the About
+ * dialog box.
+ */
+QString getAppFullName() {
+	return QString("PlasmaLenz");
+}
+
+/**
+ * Returns the version string for the screen saver.
+ */
+QString getAppVersion() {
+	return QString("0.8");
+}
+
